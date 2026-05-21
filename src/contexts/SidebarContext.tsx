540a0b232ea7e441fdeded1208c2773907a7fd4a
Gureum <67192959+huheyun@users.noreply.github.com>
2026-05-21 16:50:10 +0900

    refactor: 빌드 오류 수정

--- src/contexts/SidebarContext.tsx
+++ src/contexts/SidebarContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, useState } from "react";
+import type { ReactNode } from "react";
 
 type SidebarContextType = {
   open: boolean;
